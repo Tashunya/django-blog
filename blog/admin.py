de0a7570d646fa445113d6f5b9d98ff4b5c2b191
Tashunya <tashunya_17@mail.ru>
2019-10-01 20:53:13 +0300

    Change admin site Post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date', 'published_date')
+    list_filter = ('created_date', 'published_date', 'author')
+    search_fields = ('title', 'text')
+    date_hierarchy = 'published_date'
+    ordering = ('published_date', )
+    raw_id_fields = ('author',)
+
+
 admin.site.register(Comment)
