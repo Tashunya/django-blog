d29d28ca86c3ecce59361af5703a7d2a805e89c5
Tashunya <tashunya_17@mail.ru>
2019-10-02 18:50:35 +0300

    Fix registration views

--- account/views.py
+++ account/views.py
@@ -1,29 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return HttpResponse('Authenticated successfully')
-            else:
-                return HttpResponse('Disabled account')
-        else:
-            return HttpResponse('Invalid login')
-    else:
-        form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request,
+#                                 username=cd['username'],
+#                                 password=cd['password'])
+#         if user is not None:
+#             if user.is_active:
+#                 login(request, user)
+#                 return HttpResponse('Authenticated successfully')
+#             else:
+#                 return HttpResponse('Disabled account')
+#         else:
+#             return HttpResponse('Invalid login')
+#     else:
+#         form = LoginForm()
+#     return render(request, 'account/login.html', {'form': form})
 
 
 def register(request):
@@ -32,8 +34,8 @@ def register(request):
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
-            Profile.objects.create(user=new_user)
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request, 'account/register_done.html',
                           {'new_user': new_user})
     else:
